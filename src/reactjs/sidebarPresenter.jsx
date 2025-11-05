c11c07e5539d9de0b3b0f333be1b223144f04a94
edsr <edsr@kth.se>
2025-11-05 23:12:02 +0100

    1.5 Presenters handle custom events fired by Views DONE

--- src/reactjs/sidebarPresenter.jsx
+++ src/reactjs/sidebarPresenter.jsx
@@ -1,25 +1,31 @@
 import { observer } from "mobx-react-lite";
 import { SidebarView } from "/src/views/sidebarView.jsx";
 
-function numberChangeButtonACB(){
-    console.log("Number change custom event fired")
-}
 
-function dishClickACB(){
-    console.log("Dish click custom event fired")
-}
-
-function dishRemoveACB(){
-    console.log("Dish remove custom event fired")
-}
 
 const Sidebar = observer(             // needed for the presenter to update (its view) when relevant parts of the model change
     function SidebarRender(props){
+        
+        function numberChangeButtonACB(number){
+            //console.log("Number change custom event fired")
+            props.model.setNumberOfGuests(number)
+        }
+
+        function dishClickACB(dish){
+            //console.log("Dish click custom event fired")
+            props.model.setCurrentDishId(dish.id)
+        }
+
+        function dishRemoveButtonACB(dish){
+            //console.log("Dish remove custom event fired")
+            props.model.removeFromMenu(dish)
+        }
+        
         return <SidebarView number={props.model.numberOfGuests} 
                             dishes={props.model.dishes}
                             onNumberChange={numberChangeButtonACB}
                             onDishClick={dishClickACB}
-                            onDishRemove={dishRemoveACB}/>;
+                            onDishRemove={dishRemoveButtonACB}/>;
     }
 );
 
