bf4a2a040c0de0f8ed64d0124c39443a1debadf6
edsr <edsr@kth.se>
2025-11-05 02:04:34 +0100

    1.2.1 Bootstraping Done

--- src/mobxReactiveModel.js
+++ src/mobxReactiveModel.js
@@ -2,7 +2,8 @@ import "/src/teacherFetch.js"; // protection against fetch() in infinite loops
 import { observable, configure } from "mobx";
 configure({ enforceActions: "never", });  // we don't use Mobx actions in the Lab
 
-export const reactiveModel="make a reactive object out of the model exported from DinnerModel";
+import {model} from "./DinnerModel.js" // We import the DinnnerModel
+export const reactiveModel= observable(model) //"make a reactive object out of the model exported from DinnerModel";
 
 
 
--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -1,7 +1,12 @@
-import {createElement, Fragment} from "react";
+import React, {createElement, Fragment} from "react";
 window.React= {createElement:createElement, Fragment:Fragment}; // needed in the lab because it works with both React and Vue
 
 import { createRoot } from "react-dom/client";
 
+import {ReactRoot} from "./ReactRoot.jsx" // We import the root component
+const rootJSX = <ReactRoot model={reactiveModel}/>
+import {reactiveModel} from "/src/mobxReactiveModel.js" // We import the reactive model
+
 // mount the app in the browser page. Test at http://localhost:8080/react.html
-createRoot(document.getElementById('TODO')).render("Hello React world!");
+// and render the reactive model
+createRoot(document.getElementById('root')).render(rootJSX);//render("Hello React world!");
