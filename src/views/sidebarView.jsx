aebc3b91054e5de43eb2e38b8e50acda69fc0c0a
edsr <edsr@kth.se>
2025-11-05 02:53:53 +0100

    1.2.3 Basic Rendering Views DONE

--- src/views/sidebarView.jsx
+++ src/views/sidebarView.jsx
@@ -1,3 +1,10 @@
 export function SidebarView(props){
-    return "SidebarView stub: number is "+props.number + " and we have "+props.dishes.length+ " dishes";
+    //return "SidebarView stub: number is "+props.number + " and we have "+props.dishes.length+ " dishes";
+    return (
+    <div>
+        <button disabled={props.number<=1}>-</button>
+        <div>{props.number}</div>
+        <button>+</button>
+    </div>
+    )
 }
