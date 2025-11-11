46669adf59b3e9f1b9d3ecf8d57afd7e89d41d41
edsr <edsr@kth.se>
2025-11-11 17:57:18 +0100

    2.4.1 DONE + npm update to 11.6.2

--- src/views/suspenseView.jsx
+++ src/views/suspenseView.jsx
@@ -1,5 +1,13 @@
 import "/src/style.css"
 
-export function SuspenseView(props) {
-    return (<div>"__Suspense View__"</div>);
+ export function SuspenseView(props) {
+    //return (<div>"__Suspense View__"</div>);
+
+    return (
+        <div>
+            {!props.promise && <span>no data</span>}
+            {props.promise && props.error && <span>{props.error.toString()}</span> }
+            {props.promise && !props.error && <img src="https://brfenergi.se/iprog/loading.gif"/>}
+        </div>
+    )
 }
