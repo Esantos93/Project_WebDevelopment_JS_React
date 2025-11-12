985a367cdb4f849aa1ba373f232c8727210f0d34
edsr <edsr@kth.se>
2025-11-12 12:08:50 +0100

    Minor changes on comments

--- src/mobxReactiveModel.js
+++ src/mobxReactiveModel.js
@@ -5,21 +5,23 @@ configure({ enforceActions: "never", });  // we don't use Mobx actions in the La
 import {model} from "./DinnerModel.js" // We import the DinnnerModel
 export const reactiveModel= observable(model) //"make a reactive object out of the model exported from DinnerModel";
 
-// Initial search
-reactiveModel.doSearch({})
-
 // Reactive Object side effect exercise (TW 2.2.3)
 reaction(function relevantSelectionACB() { return reactiveModel.currentDishId; },
         function sideEffectACB() { return reactiveModel.currentDishEffect(); })
 
 
 
-// ------ for Lab debug purposes ----------
-// making the reactive model available at the browser JavasScript Console
-window.myModel= reactiveModel;
+// To see the change from SuspenseView to SearchResultsView when the App (re)loads (TW2.3.1) --> Initial EMPTY search 
+reactiveModel.doSearch({})
 
 // To see the change from SuspenseView to DetailsView when the App reloads (TW2.3.1)
 // myModel.currentDishId=715446  --> DEACTIVATED ON TW2.4.5
+
+
+
+// ------ for Lab debug purposes ----------
+// making the reactive model available at the browser JavasScript Console
+window.myModel= reactiveModel;
 
 // making some example dishes available 
 import {dishesConst} from "/src/dishesConst.js";
