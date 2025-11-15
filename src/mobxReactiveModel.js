fb5c06d5283423fd16615f578e62900fd1b65ae3
edsr <edsr@kth.se>
2025-11-15 16:08:50 +0100

    Comments adjustment + persistence function invoked in reactive model setup

--- src/mobxReactiveModel.js
+++ src/mobxReactiveModel.js
@@ -1,32 +1,36 @@
 import "/src/teacherFetch.js"; // protection against fetch() in infinite loops
 import { observable, configure, reaction } from "mobx";
+import {model} from "./DinnerModel.js" // We import the DinnnerModel
+import { connectToPersistence } from "./firestoreModel.js"; // To make our model persistent
+import {dishesConst} from "/src/dishesConst.js";
+
+
+ /*     Our REACTIVE MODEL      */
+
+export const reactiveModel= observable(model) // "make a reactive object out of the model exported from DinnerModel";
 configure({ enforceActions: "never", });  // we don't use Mobx actions in the Lab
 
-import {model} from "./DinnerModel.js" // We import the DinnnerModel
-export const reactiveModel= observable(model) //"make a reactive object out of the model exported from DinnerModel";
 
-import { connectToPersistence } from "./firestoreModel.js";
+/*     SIDE EFFECTS installation     */                 // BOOTSTRAPING #4: Install side effects
 
 // Reactive Object side effect exercise (TW 2.2.3)
 reaction(function relevantSelectionACB() { return reactiveModel.currentDishId; },
         function sideEffectACB() { return reactiveModel.currentDishEffect(); })
 
 
+/*      PERSISTENCE of our reactive model       */
 
-// To see the change from SuspenseView to SearchResultsView when the App (re)loads (TW2.3.1) --> Initial EMPTY search 
-reactiveModel.doSearch({})
-
-// To see the change from SuspenseView to DetailsView when the App reloads (TW2.3.1)
-// myModel.currentDishId=715446  --> DEACTIVATED ON TW2.4.5
+connectToPersistence(reactiveModel, reaction)
 
 
+/*      INITIAL SEARCH          */              //BOOTSTRAPING #5
 
-// ------ for Lab debug purposes ----------
-// making the reactive model available at the browser JavasScript Console
-window.myModel= reactiveModel;
+ reactiveModel.doSearch({}) // To see the change from SuspenseView to SearchResultsView when the App (re)loads (TW2.3.1)
+// reactiveModel.currentDishId=715446 --> DEACTIVATED ON TW2.4.5 // To see the change from SuspenseView to DetailsView when the App reloads (TW2.3.1)
 
-// making some example dishes available 
-import {dishesConst} from "/src/dishesConst.js";
-window.dishesConst= dishesConst;
 
+/*      LAB DEBUGGING   */
+
+window.myModel= reactiveModel; // making the reactive model available at the browser JavasScript Console
 //myModel.addToMenu(dishesConst[2]); //You can test with more/different dishes
+window.dishesConst= dishesConst; // making some example dishes available 
--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -2,16 +2,14 @@ import React, {createElement, Fragment} from "react";
 window.React= {createElement:createElement, Fragment:Fragment}; // needed in the lab because it works with both React and Vue
 
 import { createRoot } from "react-dom/client";
-
 import {ReactRoot} from "./ReactRoot.jsx" // We import the root component
 import {reactiveModel} from "/src/mobxReactiveModel.js" // We import the reactive model
-const rootJSX = <ReactRoot model={reactiveModel}/>
+
+// BOOTSTRAPING #1: We instantite our REACTIVE model
+// BOOTSTRAPING #2: We pass the reactive model to the ROOT
+const rootJSX = <ReactRoot model={reactiveModel}/> 
+
 
 // mount the app in the browser page. Test at http://localhost:8080/react.html
 // and render the reactive model
-createRoot(document.getElementById('root')).render(rootJSX);//render("Hello React world!");
-
-// To see the change from SuspenseView to DetailsView when the App reloads (TW2.3.1)
-//myModel.currentDishId=715446 
-// To see the change from SuspenseView to SearchResultsView when the App (re)loads (TW2.3.1)
-//myModel.doSearch()
+createRoot(document.getElementById('root')).render(rootJSX);//render("Hello React world!"); BOOTSTRAPING #3: We mount ROOT
